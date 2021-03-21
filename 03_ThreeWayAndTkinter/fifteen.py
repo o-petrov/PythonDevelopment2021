9e2b147c1a54b1f14f579c5c35697f666706f470
Oleg Petrov <o.m.petrov@yandex.ru>
2021-03-21 19:13:31 +0300

    make new game (shuffle) and exit work

--- 03_ThreeWayAndTkinter/fifteen.py
+++ 03_ThreeWayAndTkinter/fifteen.py
@@ -1,5 +1,6 @@
-import time
 import tkinter as tk
+from itertools import product
+from random import shuffle
 
 
 class Application(tk.Frame):
@@ -8,19 +9,17 @@ class Application(tk.Frame):
         self.rows=4
         self.cols=4
         self.buttons=self.rows*self.cols-1
+        self.pos = list(product(range(1, self.rows+1), range(self.cols)))
         self.create_widgets()
+        self.grid_reconfigure()
+        self.new_game()
 
     def create_widgets(self):
-        self.new = tk.Button(self, text='New')
+        self.new = tk.Button(self, text='New', command=self.new_game)
         self.new.grid(row=0, column=0, columnspan=2)
-        self.exit = tk.Button(self, text='Exit')
+        self.exit = tk.Button(self, text='Exit', command=self.quit)
         self.exit.grid(row=0, column=2, columnspan=2)
-
         self.button = [tk.Button(self, text=str(i+1)) for i in range(self.buttons)]
-        for i in range(self.buttons):
-            self.button[i].grid(row=1+i//self.rows, column=i%self.rows, sticky="NSEW")
-
-        self.grid_reconfigure()
 
     def grid_reconfigure(self):
         self.master.rowconfigure(0, weight=1)
@@ -31,6 +30,11 @@ class Application(tk.Frame):
             self.rowconfigure(i+1, weight=1)
         for i in range(self.cols):
             self.columnconfigure(i, weight=1)
+
+    def new_game(self):
+        shuffle(self.pos)
+        for b, (r, c) in zip(self.button, self.pos):
+            b.grid(row=r, column=c, sticky="NSEW")
 
 
 if (__name__ == "__main__"):
